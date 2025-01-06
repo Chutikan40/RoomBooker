85ad3735fd04da48651ee482d16e27eca54d3a1d
Chutikan40 <chutikanforwork40@gmail.com>
2025-01-07 00:03:49 +0700

    Add project files.

---
+++ RoomBooker/Areas/Identity/Data/RoomBookerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RoomBooker.Areas.Identity.Data;
+
+namespace RoomBooker.Areas.Identity.Data;
+
+public class RoomBookerContext : IdentityDbContext<RoomBookerUser>
+{
+    public RoomBookerContext(DbContextOptions<RoomBookerContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
